f58efa04ce004b0852248e4cb567200d359e0737
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-05-28 17:58:08 +0200

          

---
+++ android/build/generated/jni/ti.accountmanager.AccountManagerModule.cpp
@@ -0,0 +1,270 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "ti.accountmanager.AccountManagerModule.h"
+
+#include "AndroidUtil.h"
+#include "EventEmitter.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "Proxy.h"
+#include "ProxyFactory.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "AccountManagerModule"
+
+using namespace v8;
+
+		namespace ti {
+		namespace accountmanager {
+
+
+Persistent<FunctionTemplate> AccountManagerModule::proxyTemplate = Persistent<FunctionTemplate>();
+jclass AccountManagerModule::javaClass = NULL;
+
+AccountManagerModule::AccountManagerModule(jobject javaObject) : titanium::Proxy(javaObject)
+{
+}
+
+void AccountManagerModule::bindProxy(Handle<Object> exports)
+{
+	if (proxyTemplate.IsEmpty()) {
+		getProxyTemplate();
+	}
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("Accman2");
+
+	Local<Function> proxyConstructor = proxyTemplate->GetFunction();
+	Local<Object> moduleInstance = proxyConstructor->NewInstance();
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void AccountManagerModule::dispose()
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Dispose();
+		proxyTemplate = Persistent<FunctionTemplate>();
+	}
+
+	titanium::KrollModule::dispose();
+}
+
+Handle<FunctionTemplate> AccountManagerModule::getProxyTemplate()
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate;
+	}
+
+	LOGD(TAG, "GetProxyTemplate");
+
+	javaClass = titanium::JNIUtil::findClass("ti/accountmanager/AccountManagerModule");
+	HandleScope scope;
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("Accman2");
+
+	Handle<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		titanium::KrollModule::getProxyTemplate()
+, javaClass, nameSymbol);
+
+	proxyTemplate = Persistent<FunctionTemplate>::New(t);
+	proxyTemplate->Set(titanium::Proxy::inheritSymbol,
+		FunctionTemplate::New(titanium::Proxy::inherit<AccountManagerModule>)->GetFunction());
+
+	titanium::ProxyFactory::registerProxyPair(javaClass, *proxyTemplate);
+
+	// Method bindings --------------------------------------------------------
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "getAuthToken", AccountManagerModule::getAuthToken);
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "getAccounts", AccountManagerModule::getAccounts);
+
+	Local<ObjectTemplate> prototypeTemplate = proxyTemplate->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = proxyTemplate->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return proxyTemplate;
+}
+
+// Methods --------------------------------------------------------------------
+Handle<Value> AccountManagerModule::getAuthToken(const Arguments& args)
+{
+	LOGD(TAG, "getAuthToken()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(AccountManagerModule::javaClass, "getAuthToken", "(Ljava/lang/String;Ljava/lang/String;Ljava/util/HashMap<java/lang/String,java/lang/Object>;)Ljava/lang/String;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getAuthToken' with signature '(Ljava/lang/String;Ljava/lang/String;Ljava/util/HashMap<java/lang/String,java/lang/Object>;)Ljava/lang/String;'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	if (args.Length() < 3) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "getAuthToken: Invalid number of arguments. Expected 3 but got %d", args.Length());
+		return ThrowException(Exception::Error(String::New(errorStringBuffer)));
+	}
+
+	jvalue jArguments[3];
+
+
+
+
+	
+	
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	
+	
+	if (!args[1]->IsNull()) {
+		Local<Value> arg_1 = args[1];
+		jArguments[1].l =
+			titanium::TypeConverter::jsValueToJavaString(env, arg_1);
+	} else {
+		jArguments[1].l = NULL;
+	}
+
+	bool isNew_2;
+	
+	if (!args[2]->IsNull()) {
+		Local<Value> arg_2 = args[2];
+		jArguments[2].l =
+			titanium::TypeConverter::jsValueToJavaObject(env, arg_2, &isNew_2);
+	} else {
+		jArguments[2].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	jstring jResult = (jstring)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+				env->DeleteLocalRef(jArguments[1].l);
+
+
+			if (isNew_2) {
+				env->DeleteLocalRef(jArguments[2].l);
+			}
+
+
+	if (env->ExceptionCheck()) {
+		Handle<Value> jsException = titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+		return jsException;
+	}
+
+	if (jResult == NULL) {
+		return Null();
+	}
+
+	Handle<Value> v8Result = titanium::TypeConverter::javaStringToJsString(env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	return v8Result;
+
+}
+Handle<Value> AccountManagerModule::getAccounts(const Arguments& args)
+{
+	LOGD(TAG, "getAccounts()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(AccountManagerModule::javaClass, "getAccounts", "()[Lorg/appcelerator/kroll/KrollDict;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getAccounts' with signature '()[Lorg/appcelerator/kroll/KrollDict;'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	jvalue* jArguments = 0;
+
+	jobject javaProxy = proxy->getJavaObject();
+	jobject jResult = (jobject)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+	if (env->ExceptionCheck()) {
+		Handle<Value> jsException = titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+		return jsException;
+	}
+
+	if (jResult == NULL) {
+		return Null();
+	}
+
+	Handle<Value> v8Result = titanium::TypeConverter::javaObjectToJsValue(env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	return v8Result;
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+
+		} // accountmanager
+		} // ti
---
+++ android/src/ti/accountmanager/AccountManagerModule.java
@@ -0,0 +1,113 @@
+package ti.accountmanager;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.common.Log;
+import org.appcelerator.kroll.common.TiConfig;
+import org.appcelerator.titanium.TiApplication;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AuthenticatorDescription;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+
+@Kroll.module(name = "Accman2", id = "org.bcbhh")
+public class AccountManagerModule extends KrollModule {
+
+	private static final String ACCOUNT_NAME = "name";
+	private static final String ACCOUNT_TYPE = "type";
+	private static final String ACCOUNT_TYPE_LABEL = "accountType";
+
+	// Standard Debugging variables
+	private static final String LCAT = "Accman2Module";
+	private static AccountManager mAccountManager;
+
+	private static Map<String, AuthenticatorDescription> _authenticatorLookup;
+	private static Resources _resources;
+	private static PackageManager mPackageManager;
+
+	// You can define constants with @Kroll.constant, for example:
+	// @Kroll.constant public static final String EXTERNAL_NAME = value;
+
+	public AccountManagerModule() {
+		super();
+	}
+
+	@Kroll.onAppCreate
+	public static void onAppCreate(TiApplication app) {
+
+		mPackageManager = app.getPackageManager();
+		_resources = app.getResources();
+		mAccountManager = AccountManager.get(app.getApplicationContext());
+		AuthenticatorDescription[] accTypes = mAccountManager.getAuthenticatorTypes();
+		_authenticatorLookup = new HashMap<String, AuthenticatorDescription>();
+		for (AuthenticatorDescription authDesc : accTypes) {
+			_authenticatorLookup.put(authDesc.type, authDesc);
+		}
+		Log.d(LCAT, "inside onAppCreate");
+	}
+
+	@Kroll.method
+	public KrollDict[] getAccounts() {
+
+		Account[] accounts = mAccountManager.getAccounts();
+		KrollDict[] accountList = new KrollDict[accounts.length];
+		int idx = 0;
+		for (Account account : accounts) {
+			KrollDict accountDict = new KrollDict();
+			accountDict.put(ACCOUNT_NAME, account.name);
+			accountDict.put(ACCOUNT_TYPE, account.type);
+			accountDict.put(ACCOUNT_TYPE_LABEL,
+					labelForAuthenticator(_authenticatorLookup
+							.get(account.type)));
+			accountList[idx] = accountDict;
+			idx++;
+		}
+		return accountList;
+	}
+
+	@Kroll.method
+	public String getAuthToken(String accountName, String accountType,
+			HashMap<String,Object> params) {
+		Account[] accounts = mAccountManager.getAccountsByType(accountType);
+		for (Account account : accounts) {
+			if (accountName.equals(account.name)) {
+				String token = null;
+				try {
+					Log.e(LCAT, "Trying to get token for " + accountName);
+					token = mAccountManager.blockingGetAuthToken(account, "", true);
+					Log.e(LCAT, "After blocking call");
+				} catch (Exception e) {
+				
+					e.printStackTrace();
+
+				}
+				Log.e(LCAT, "Token returned" + token);
+				return token;
+			}
+		}
+		return null;
+	}
+
+	private String labelForAuthenticator(
+			AuthenticatorDescription authenticatorDescription) {
+		Resources resources;
+		try {
+			resources = mPackageManager
+					.getResourcesForApplication(authenticatorDescription.packageName);
+		} catch (NameNotFoundException e) {
+			return authenticatorDescription.type;
+		}
+		return resources.getString(authenticatorDescription.labelId);
+	}
+
+}
